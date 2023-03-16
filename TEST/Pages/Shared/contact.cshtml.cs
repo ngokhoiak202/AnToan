b397f6d13d888408d4b9d74e9bd85a4e9b94ea06
AnhKhoi202 <2051050219khoi@ou.edu.vn>
2023-03-17 03:58:55 +0700

    test

---
+++ TEST/Pages/Shared/contact.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TEST.Pages.Shared
+{
+    public class contactModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
